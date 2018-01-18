6c13a327a200c7db0bf2fe8aaf0b5c78f8315b36
Bradley Ellert <bellert@dwavesys.com>
2018-01-17 17:45:59 -0800

    Update to use pandas instead of numpy

--- verifygate.py
+++ verifygate.py
@@ -1,25 +1,12 @@
-import numpy as np
+import pandas as pd
 import dimod
 
 
-def verifygate(pmodel, aux, func):
-    """
-    pmodel
-    aux - index to start of variables to rollup (SHOULD BE NAMED INSTEAD OF NUMBERED)
-    func - what to evaluate to determine validity of combination
-    """
+def verifygate(pmodel, vars):
     es = dimod.ExactSolver()
     resp = es.sample_ising(pmodel.model.linear, pmodel.model.quadratic)
     resp = resp.as_binary()
 
-    num = len(pmodel.model.linear)
+    df = pd.DataFrame([dict(data['sample'], **{'energy': data['energy']}) for data in resp.data()])
 
-    arr = labels = np.empty(shape=(2,) * num)
-    for data in resp.data():
-        arr[tuple(data['sample'].values())] = data['energy']
-    rollup = np.min(arr, axis=tuple(range(aux, num)))
-    for data in resp.data():
-        args = tuple(data['sample'].values())[:aux]
-        labels[args] = func(*args)
-
-    return rollup, labels
+    return df.groupby(vars).energy.agg(min)
