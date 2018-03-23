a9bbe18a3b30b3baaef2105b0d34da606cde5c89
Bradley Ellert <bellert@dwavesys.com>
2018-03-23 12:42:08 -0700

    Add template setup script

---
+++ setup.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+
+import sys
+import os
+from setuptools import setup
+
+_PY2 = sys.version_info.major == 2
+
+package_name = 'PROJECTNAME'
+
+# Change directories so this works when called from other locations. Useful in build systems that build from source.
+setup_folder_loc = os.path.dirname(os.path.abspath(__file__))
+os.chdir(setup_folder_loc)
+
+# Add __version__, __author__, __authoremail__, __description__ to this namespace
+path_to_package_info = os.path.join('.', 'dwave', package_name, 'package_info.py')
+if _PY2:
+    execfile(path_to_package_info)
+else:
+    exec(open(path_to_package_info).read())
+
+# These should be minimal requiments for the package to work, and avoid pinning dependencies unless required. See
+# https://packaging.python.org/discussions/install-requires-vs-requirements/
+install_requires = []
+
+# Any extra requirements, to be used by pip install PACKAGENAME['keyname']
+extras_require = {}
+
+# The packages in this repo that are to be installed. Either list these explictly, or use setuptools.find_packages. If
+# the latter, take care to filter unwanted packages (e.g. tests)
+packages = ['dwave.{package_name}'.format(package_name=package_name)]
+
+setup(
+    name=package_name,
+    version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
+    long_description=open('README.rst').read(),
+    url='https://github.com/dwavesystems/{package_name}'.format(package_name=package_name),
+    license='Apache 2.0',
+    packages=packages,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    # Required to make this a namespace package. If this package is at lower level, include all packages above it.
+    namespace_packages=['dwave']
+)
