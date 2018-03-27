75d02958c92e815811ad253bf1d6adda36cc275a
Bradley Ellert <bellert@dwavesys.com>
2018-03-27 12:27:46 -0700

    Input basic package info

--- factoring/package_info.py
+++ factoring/package_info.py
@@ -1,4 +1,4 @@
-__version__ = ''
+__version__ = '0.0.1'
 __author__ = 'D-Wave Systems Inc.'
-__authoremail__ = ''
-__description__ = ''
+__authoremail__ = 'bellert@dwavesys.com'
+__description__ = 'Demo of factoring a 6-bit integer by embedding a 3-bit multiplier on the D-Wave QPU'
