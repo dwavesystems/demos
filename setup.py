ef7d4e878701eddaa6903719f401263a7f72aaf2
Bradley Ellert <bellert@dwavesys.com>
2018-04-30 15:24:38 -0700

    Still need penaltymodel-cache

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 # These should be minimal requiments for the package to work, and avoid pinning dependencies unless required. See
 # https://packaging.python.org/discussions/install-requires-vs-requirements/
-install_requires = [#'penaltymodel-cache==0.2.0',
+install_requires = ['penaltymodel-cache==0.2.0',
                     'dimod==0.6.8',
                     'dwave-system==0.2.5',
                     'networkx==2.0']
