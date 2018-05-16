e460fbbb1d20321b934686586658c466104ea9dd
Bradley Ellert <bellert@dwavesys.com>
2018-05-16 13:44:33 -0700

    Update drawing function
    Accept node_link_data
    Draw whether it's been solved or not

--- dwave_structural_imbalance_demo/drawing.py
+++ dwave_structural_imbalance_demo/drawing.py
@@ -1,39 +1,56 @@
+from collections import defaultdict
+from itertools import product
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def draw(filename, S, imbalance, coloring, position=None):
+def draw(filename, node_link_data, position=None):
     """Plot the given signed social network.
 
     Args:
         filename: The name of the file to be generated.
-        S: The network
-        imbalance: The set of frustrated edges.
-        coloring: A two-coloring of the network
+        node_link_data: The network represented as returned by nx.node_link_data()
         position (optional): The position for the nodes.
 
     """
 
-    if position is None:
-        position = nx.circular_layout(S)
+    S = nx.node_link_graph(node_link_data)
 
     # we need a consistent ordering of the edges
     edgelist = S.edges()
     nodelist = S.nodes()
 
+    if position is None:
+        try:
+            # group bipartition if nodes are colored
+            dist = defaultdict(dict)
+            for u, v in product(nodelist, repeat=2):
+                if u == v: # node has no distance from itself
+                    dist[u][v] = 0
+                elif nodelist[u]['color'] == nodelist[v]['color']: # make same color nodes closer together
+                    dist[u][v] = 1
+                else: # make different color nodes further apart
+                    dist[u][v] = 2
+            position = nx.kamada_kawai_layout(S, dist)
+        except KeyError:
+            # default to circular layout if nodes aren't colored
+            position = nx.circular_layout(S)
+
     # get the colors assigned to each edge based on friendly/hostile
-    sign_edge_color = [-S[u][v]['sign'] for u, v in edgelist]
-    sign_node_color = [0 for __ in nodelist]
-    sign_edge_style = ['solid' if S[u][v]['sign'] > 0 else 'dotted' for u, v in edgelist]
+    sign_edge_color = [S[u][v]['sign'] for u, v in edgelist]
 
     # get the colors assigned to each node by coloring
-    coloring_node_color = [-2 * coloring[v] + 1 for v in nodelist]
+    try:
+        coloring_node_color = [-2 * nodelist[v]['color'] + 1 for v in nodelist]
+    except KeyError:
+        coloring_node_color = [0 for __ in nodelist]
 
-    # get the colors of the violated edges
-    c1 = 1  # grey
-    c2 = .61  # ochre
-    coloring_edge_color = [c2 if (u, v) in imbalance else c1 for u, v in edgelist]
-    edge_violation_style = ['dotted' if (u, v) in imbalance else 'solid' for u, v in edgelist]
+    # get the styles of the violated edges
+    try:
+        edge_violation_style = ['dotted' if S[u][v]['frustrated'] else 'solid' for u, v in edgelist]
+    except KeyError:
+        edge_violation_style = ['solid' for __ in edgelist]
 
     # draw the the coloring
     nx.draw(S,
@@ -45,9 +62,13 @@ def draw(filename, S, imbalance, coloring, position=None):
             edgelist=edgelist, nodelist=nodelist,
             width=2,
             node_color=coloring_node_color,
-            edge_color=coloring_edge_color,
-            edge_cmap=plt.get_cmap('nipy_spectral'),
+            edge_color=sign_edge_color,
+            edge_cmap=plt.get_cmap('RdYlGn'),
+            edgecolors='black',
+            style=edge_violation_style
             )
 
     plt.savefig(filename, facecolor='white', dpi=500)
     plt.clf()
+
+    return position
