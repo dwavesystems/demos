c4a06c4e5c02b65492657fea76a9b0488b343044
Bradley Ellert <bellert@dwavesys.com>
2018-05-18 13:05:16 -0700

    0.0.1 --> 0.0.2

--- dwave_structural_imbalance_demo/package_info.py
+++ dwave_structural_imbalance_demo/package_info.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'bellert@dwavesys.com'
 __description__ = 'Demo for analyzing the structural imbalance on a signed social network.'
