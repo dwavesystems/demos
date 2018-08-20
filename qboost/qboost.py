81c3646f7810064b8e19aff83d848dddf1d433a7
Patrick Huembeli <patrick.huembeli@icfo.es>
2018-08-20 12:08:44 -0400

    Proposed changes implemented

--- qboost/qboost.py
+++ qboost/qboost.py
@@ -18,17 +18,16 @@ from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, AdaBoos
 import numpy as np
 from copy import deepcopy
 
-def weight_penalty(h, y, percent = 0.1): 
+def weight_penalty(prediction, y, percent = 0.1): 
     """
-    Penalize differences of the prediction from the label.
+    For Regression we have to introduce a metric to penalize differences of the prediction from the label y.
+    Percent gives the maximum deviation of the prediction from the label that is not penalized.
     """
-    diff = np.abs(h-y)
-    minn = diff.min()
-    maxx = diff.max()
-    norm = (diff-minn)/(maxx-minn)
-    norm = 1.0*(norm  < percent)
-#     h, y = np.expm1(h), np.expm1(y)
-    
+    diff = np.abs(prediction-y)
+    min_ = diff.min()
+    max_ = diff.max()
+    norm = (diff-min_)/(max_-min_)
+    norm = 1.0*(norm  < percent)  
     return norm
 
 class WeakClassifiers(object):
@@ -91,17 +90,17 @@ class WeakClassifiers(object):
 
     def copy(self):
 
-        clf = WeakClassifiers(n_estimators=self.n_estimators, max_depth=self.max_depth)
-        clf.estimators_ = deepcopy(self.estimators_)
+        classifier = WeakClassifiers(n_estimators=self.n_estimators, max_depth=self.max_depth)
+        classifier.estimators_ = deepcopy(self.estimators_)
         if hasattr(self, 'estimator_weights'):
-            clf.estimator_weights = np.array(self.estimator_weights)
+            classifier.estimator_weights = np.array(self.estimator_weights)
 
-        return clf
+        return classifier
 
 
 class QBoostClassifier(WeakClassifiers):
     """
-    Qboost
+    Qboost Classifier
     """
     def __init__(self, n_estimators=50, max_depth=3):
         super(QBoostClassifier, self).__init__(n_estimators=n_estimators,
@@ -137,24 +136,21 @@ class QBoostClassifier(WeakClassifiers):
         samples = np.array([[samp[k] for k in range(self.n_estimators)] for samp in res])
 
         # take the optimal solution as estimator weights
-        # self.estimator_weights = np.mean(samples, axis=0)
         self.estimator_weights = samples[0]
 
     def predict(self, X):
-        import numpy as np
-
         n_data = len(X)
         pred_all = np.array([h.predict(X) for h in self.estimators_])
         temp1 = np.dot(self.estimator_weights, pred_all)
         T1 = np.sum(temp1, axis=0) / (n_data * self.n_estimators * 1.)
-        y = np.sign(temp1 - T1)
+        y = np.sign(temp1 - T1) #binary classes are either 1 or -1
 
         return y
 
 
 class WeakRegressor(object):
     """
-    Weak Classifiers based on DecisionTree
+    Weak Regressor based on DecisionTreeRegressor
     """
 
     def __init__(self, n_estimators=50, max_depth=3, DT = True, Ada = False, ):
@@ -162,11 +158,6 @@ class WeakRegressor(object):
         self.estimators_ = []
         self.max_depth = max_depth
         self.__construct_wc()
-        self.Qu = 0.0
-        self.hij = 0.0
-        self.var1 = 0.0
-        self.qij = 0.0
-        self.norm = 0.0
 
     def __construct_wc(self):
 
@@ -184,20 +175,18 @@ class WeakRegressor(object):
         :return:
         """
 
-        self.estimator_weights = np.zeros(self.n_estimators)
+        self.estimator_weights = np.zeros(self.n_estimators) #initialize all estimator weights to zero
 
         d = np.ones(len(X)) / len(X)
-        for i, h in enumerate(self.estimators_):
-            print(i)
+        for i, h in enumerate(self.estimators_): #fit all estimators
             h.fit(X, y, sample_weight=d)
             pred = h.predict(X)
+            # For classification one simply compares (pred != y)
+            # For regression we have to define another metric
             norm = weight_penalty(pred, y)
-            self.norm = norm
             eps = d.dot(norm)
             if eps == 0: # to prevent divided by zero error
                 eps = 1e-20
-#            if eps == 1.0:
-#                eps = 1.0 - 1e-20
             w = (np.log(1 - eps) - np.log(eps)) / 2
             d = d * np.exp(- w * y * pred)
             d = d / d.sum()
@@ -224,21 +213,25 @@ class WeakRegressor(object):
 
     def copy(self):
 
-        clf = WeakRegressor(n_estimators=self.n_estimators, max_depth=self.max_depth)
-        clf.estimators_ = deepcopy(self.estimators_)
+        classifier = WeakRegressor(n_estimators=self.n_estimators, max_depth=self.max_depth)
+        classifier.estimators_ = deepcopy(self.estimators_)
         if hasattr(self, 'estimator_weights'):
-            clf.estimator_weights = np.array(self.estimator_weights)
+            classifier.estimator_weights = np.array(self.estimator_weights)
 
-        return clf
+        return classifier
 
 
 class QBoostRegressor(WeakRegressor):
     """
-    Qboost
+    Qboost Regressor
     """
     def __init__(self, n_estimators=50, max_depth=3):
         super(QBoostRegressor, self).__init__(n_estimators=n_estimators,
                                               max_depth=max_depth)
+        self.Qu = 0.0
+        self.hij = 0.0
+        self.var1 = 0.0
+        self.qij = 0.0
 
     def fit(self, X, y, sampler, lmd=0.2, **kwargs):
 
@@ -268,7 +261,6 @@ class QBoostRegressor(WeakRegressor):
                 Q[(i, j)] = qij[i, j]
 
         self.Qu = Q
-        print(Q)
         # step 3: optimize QUBO
         res = sampler.sample_qubo(Q, **kwargs)
         samples = np.array([[samp[k] for k in range(self.n_estimators)] for samp in res])
@@ -278,12 +270,9 @@ class QBoostRegressor(WeakRegressor):
         self.estimator_weights = samples[0]
 
     def predict(self, X):
-        import numpy as np
         n_data = len(X)
         pred_all = np.array([h.predict(X) for h in self.estimators_])
         temp1 = np.dot(self.estimator_weights, pred_all)
-#        T1 = np.sum(temp1, axis=0) / (n_data * self.n_estimators * 1.)
-#        y = np.sign(temp1 - T1)
         norm = np.sum(self.estimator_weights)
         if norm > 0:
             y = temp1 / norm
@@ -294,13 +283,14 @@ class QBoostRegressor(WeakRegressor):
 
 class QboostPlus(object):
     """
+    Only for Classifiers
     Quantum boost existing (weak) classifiers
     """
 
     def __init__(self, weak_classifier_list):
         self.estimators_ = weak_classifier_list
         self.n_estimators = len(self.estimators_)
-        self.estimator_weights = np.ones(self.n_estimators)
+        self.estimator_weights = np.ones(self.n_estimators) #estimator weights will be binary (Dwave output)
 
     def fit(self, X, y, sampler, lmd=0.2, **kwargs):
 
@@ -346,7 +336,7 @@ class QboostPlus(object):
 
 class QboostPlusRegression(object):
     """
-    Quantum boost existing (weak) classifiers
+    Quantum boost existing (weak) regressors
     """
 
     def __init__(self, weak_Regressor_list):
