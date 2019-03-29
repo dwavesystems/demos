53d53c103df2d526ce6fb7e55596c2d4549e6c76
Melody Wong <mwong@dwavesys.com>
2019-03-28 20:19:58 -0700

    Replace hardcoded kernel name with python version

--- qboost/tests/test_notebook.py
+++ qboost/tests/test_notebook.py
@@ -1,9 +1,11 @@
 import unittest
 import os
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
+import sys
 
+from nbconvert.preprocessors import ExecutePreprocessor
 from dwave.system.samplers import DWaveSampler
+
 
 try:
     DWaveSampler()
@@ -13,10 +15,13 @@ except ValueError:
 
 
 def run_notebook(path):
+    # Set kernel_name based on python version
+    kernel_name = "python{0}".format(sys.version_info.major)
+
     with open(path) as notebook_file:
         print('Running notebook {}'.format(path))
         nb = nbformat.read(notebook_file, nbformat.current_nbformat)
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python2', allow_errors=True)
+        ep = ExecutePreprocessor(timeout=600, kernel_name=kernel_name, allow_errors=True)
         ep.preprocess(nb, {'metadata': {'path': os.path.dirname(path)}})
 
         return nb
@@ -63,11 +68,14 @@ class TestNotebook(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print('Running modified notebook...')
+
         notebook_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'demo.ipynb')
+        kernel_name = "python{0}".format(sys.version_info.major)
+
         with open(notebook_path) as notebook_file:
             cls.notebook = nbformat.read(notebook_file, nbformat.current_nbformat)
             cls.notebook = _modify_grid_search_cell(cls.notebook)
-            ep = ExecutePreprocessor(timeout=600, kernel_name='python2', allow_errors=True)
+            ep = ExecutePreprocessor(timeout=600, kernel_name=kernel_name, allow_errors=True)
             ep.preprocess(cls.notebook, {'metadata': {'path': os.path.dirname(notebook_path)}})
 
     def test_no_errors(self):
