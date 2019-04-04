25a34542bf17d1ee87225d1136a7efb3d64845d9
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2019-04-04 10:14:57 -0700

    Tweak code a bit more

--- examples/miqubo/titanic.py
+++ examples/miqubo/titanic.py
@@ -20,9 +20,9 @@ dataset = pd.read_csv(datapath)
 
 # Define MI calculations
 def prob(dataset):
-    """Joint probability distribution for the given data."""
+    """Joint probability distribution P(X) for the given data."""
 
-    # bin by the number of different values
+    # bin by the number of different values per feature
     num_rows, num_columns = dataset.shape
     bins = [len(np.unique(dataset[:, ci])) for ci in range(num_columns)]
 
@@ -35,18 +35,24 @@ def shannon_entropy(p):
     return -sum(pi*np.log2(pi) for pi in p if pi)
 
 def conditional_shannon_entropy(p, *conditional_indices):
-    """Shannon entropy of P(X) conditional on variable j."""
+    """Conditional Shannon entropy H(X;Y) = H(X,Y) - H(Y)."""
 
     axis = tuple(i for i in np.arange(len(p.shape)) if i not in conditional_indices)
 
     return shannon_entropy(p) - shannon_entropy(np.sum(p, axis=axis))
 
 def mutual_information(prob, j):
-    """Mutual information between variables X and variable j."""
+    """Mutual information between variables X and variable Y.
+
+    Calculated as I(X; Y) = H(X) - H(X|Y)."""
+
     return shannon_entropy(np.sum(prob, axis=j)) - conditional_shannon_entropy(prob, j)
 
 def conditional_mutual_information(p, j, *conditional_indices):
-    """Mutual information between variables X and variable j conditional on variable k."""
+    """Mutual information between variables X and variable Y conditional on variable Z.
+
+    Calculated as I(X;Y|Z) = H(X|Z) - H(X|Y,Z)"""
+
     return conditional_shannon_entropy(np.sum(p, axis=j), *conditional_indices) - conditional_shannon_entropy(p, j, *conditional_indices)
 
 # Rank the MI between survival and every other variable
@@ -60,10 +66,10 @@ labels, values = zip(*sorted(scores.items(), key=lambda pair: pair[1], reverse=T
 # Plot the MI between survival and every other variable
 plt.figure()
 ax1 = plt.subplot(1, 2, 1)
-ax1.set_ylabel('MI Between Survival and Feature')
 ax1.set_title("Mutual Information")
-plt.bar(np.arange(len(labels)), values)
+ax1.set_ylabel('MI Between Survival and Feature')
 plt.xticks(np.arange(len(labels)), labels, rotation=90)
+plt.bar(np.arange(len(labels)), values)
 
 # Build a QUBO that maximizes MI between survival and a subset of features
 bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
@@ -80,7 +86,6 @@ for f0, f1 in itertools.combinations(features, 2):
 
 bqm.normalize()  # Normalize biases & interactions to the range -1, 1
 
-
 # Set up a QPU sampler with a fully-connected graph of all the variables
 qpu_sampler = DWaveSampler(solver={'qpu': True})
 
@@ -90,11 +95,12 @@ embedding = find_clique_embedding(bqm.variables,
 
 sampler = FixedEmbeddingComposite(qpu_sampler, embedding)
 
-# For each value for k, penalize selection of fewer or more features
+# For each number of features, k, penalize selection of fewer or more features
 selected_features = np.zeros((len(features), len(features)))
 for k in range(1, len(features) + 1):
     kbqm = bqm.copy()
-    kbqm.update(dimod.generators.combinations(features, k, strength=6))
+    kbqm.update(dimod.generators.combinations(features, k,
+                                              strength=6)) # Determines the penalty
 
     # sample = sampler.sample(kbqm, num_reads=10000).first.sample
     sample = dimod.ExactSolver().sample(kbqm).first.sample
@@ -102,23 +108,19 @@ for k in range(1, len(features) + 1):
     for fi, f in enumerate(features):
         selected_features[k-1, fi] = sample[f]
 
-# Plot the best feature set for each value of k
-# plt.subplot(1, 2, 2)
-# plt.imshow(selected_features)
-# plt.ylabel('number of features')
-# plt.xticks(np.arange(len(features)), features, rotation=90)
-#
-# plt.show()
+# Plot the best feature set per number of selected features
 ax2 = plt.subplot(1, 2, 2)
 ax2.set_title("Best Feature Selection")
-ax2.imshow(selected_features, cmap=colors.ListedColormap(['white', 'red']))
-ax2.set_yticks(np.arange(-0.5, len(features)), minor=True)
-ax2.set_yticks(np.arange(len(features)))
-ax2.set_yticklabels(np.arange(1, len(features)+1))
 ax2.set_ylabel('Number of Selected Features')
-ax2.set_xticks(np.arange(-0.5, len(features)), minor=True)
 ax2.set_xticks(np.arange(len(features)))
 ax2.set_xticklabels(features, rotation=90)
+ax2.set_yticks(np.arange(len(features)))
+ax2.set_yticklabels(np.arange(1, len(features)+1))
+# Set a grid on minor ticks
+ax2.set_xticks(np.arange(-0.5, len(features)), minor=True)
+ax2.set_yticks(np.arange(-0.5, len(features)), minor=True)
 ax2.grid(which='minor', color='black')
+
+ax2.imshow(selected_features, cmap=colors.ListedColormap(['white', 'red']))
 plt.tight_layout()
 plt.show()
