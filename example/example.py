ab1bf9f846b5f85ada84d44282a504df2efdaf52
Melody Wong <mwong@dwavesys.com>
2019-05-23 15:30:00 -0700

    Add colour map of Canada

--- example/example.py
+++ example/example.py
@@ -16,18 +16,49 @@ class Region:
         self.yellow = name + "_y"
 
 
-# Map colouring problem
-bc = Region("bc")
-ab = Region("ab")
-ma = Region("ma")
+# Set up provinces and neighbours
+bc = Region("bc")   # British Columbia
+ab = Region("ab")   # Alberta
+sk = Region("sk")   # Saskatchewan
+mb = Region("mb")   # Manitoba
+on = Region("on")   # Ontario
+qc = Region("qc")   # Quebec
+nl = Region("nl")   # Newfoundland and Labrador
+nb = Region("nb")   # New Brunswick
+pe = Region("pe")   # Prince Edward Island
+ns = Region("ns")   # Nova Scotia
+yt = Region("yt")   # Yukon
+nt = Region("nt")   # Northwest Territories
+nu = Region("nu")   # Nunavut
+
+provinces = [bc, ab, sk, mb, on, qc, nl, nb, pe, ns, yt, nt, nu]
+
+neighbours = [(bc, ab),
+              (bc, nt),
+              (bc, yt),
+              (ab, sk),
+              (ab, nt),
+              (sk, mb),
+              (sk, nu),
+              (sk, nt),
+              (mb, on),
+              (mb, nu),
+              (on, qc),
+              (qc, nb),
+              (qc, nl),
+              (nb, ns),
+              (yt, nt),
+              (nt, nu)]
+
+# Set up constraint
 csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.BINARY)
 
-# One colour
-for r in [bc, ab, ma]:
-    csp.add_constraint(select_one, {r.red, r.green, r.blue, r.yellow})
+# Apply one colour
+for p in provinces:
+    csp.add_constraint(select_one, {p.red, p.green, p.blue, p.yellow})
 
-# No colour sharing
-for x, y in [(bc, ab), (ab, ma)]:
+# Apply no colour sharing between neighbours
+for x, y in neighbours:
     csp.add_constraint(not_both, {x.red, y.red})
     csp.add_constraint(not_both, {x.green, y.green})
     csp.add_constraint(not_both, {x.blue, y.blue})
@@ -36,3 +67,9 @@ for x, y in [(bc, ab), (ab, ma)]:
 bqm = dwavebinarycsp.stitch(csp)
 
 solution = KerberosSampler().sample(bqm, max_iter=10, convergence=3)
+best_solution = solution.first.sample
+print(best_solution)
+
+# Verify
+is_correct = csp.check(best_solution)
+print("Does solution satisfy our constraints? {}".format(is_correct))
