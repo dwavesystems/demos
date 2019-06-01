0025231b1e91096f78a2752542371d70184d940f
Melody Wong <mwong@dwavesys.com>
2019-05-31 20:21:53 -0700

    Add template files

---
+++ template/README.rst
@@ -0,0 +1,30 @@
+<awesome demo name> Demo
+========================
+Short blurb on what this awesome demo does.
+
+Expected QPU time: <some number> <some unit of time>
+
+Code Overview
+-------------
+Give a general overview of how the code works.
+
+When possible, use bullet points so that:
+* Ideas are bite-sized
+* Ideas are quickly conveyed
+
+Code Specifics
+--------------
+Draw attention to certain parts of the code implementation.
+
+This is the place to:
+* Highlight an implentation
+* Talk about unusual or potentially difficult parts of the code
+* Explain a code decision
+
+Note: there is no need to repeat everything that is already well-documented in
+the code.
+
+References
+----------
+A. Person, "Title of Amazing Information",
+`short link name <https://example.com/>`_
---
+++ template/awesome_demo_name.py
@@ -0,0 +1,25 @@
+# Copyright [yyyy] D-Wave Systems Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+
+"""
+Things to do:
+ - Fill in [yyyy] in the license
+ - Add demo code below
+ - Format code so that it conforms with PEP 8
+ - Delete this comment :)
+"""
+
+#TODO: Do people with no affliation with D-Wave also need this license when
+# they are submitting to our Demos repo?
