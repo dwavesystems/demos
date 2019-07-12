6ec37553f1c320aacaa6da71be6ed228951c9e7e
vgoliber <victoria.goliber@gmail.com>
2019-07-12 23:13:23 +0900

    Added pipelines code

--- pipelines/pipelines.py
+++ pipelines/pipelines.py
@@ -1,4 +1,4 @@
-# Copyright [yyyy] [name of copyright owner]
+# Copyright 2019 D-Wave Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,10 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Things to do:
- - Please name this file <demo_name>.py
- - Fill in [yyyy] and [name of copyright owner] in the copyright (top line)
- - Add demo code below
- - Format code so that it conforms with PEP 8
-"""
+# Import networkx for graph tools
+import networkx as nx
+
+# Import dwave_networkx for d-wave graph tools/functions
+import dwave_networkx as dnx
+
+# Import matplotlib.pyplot to draw graphs on screen
+import matplotlib.pyplot as plt
+
+# Set the solver we're going to use
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+
+sampler = EmbeddingComposite(DWaveSampler())
+
+# Create empty graph
+G = nx.Graph()
+
+# Add edges to graph - this also adds the nodes
+G.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5), (4, 5), (4, 6), (5, 6), (6, 7)])
+
+# Find the maximum independent set, S
+S = dnx.min_vertex_cover(G, sampler=sampler, num_reads=10)
+
+# Print the solution for the user
+print('Minimum vertex cover found is', len(S))
+print(S)
+
+# Visualize the results
+k = G.subgraph(S)
+notS = list(set(G.nodes()) - set(S))
+othersubgraph = G.subgraph(notS)
+pos = nx.spring_layout(G)
+plt.figure()
+
+# Save original problem graph
+original_name = "pipelines_plot_original.png"
+nx.draw_networkx(G, pos=pos, with_labels=True)
+plt.savefig(original_name, bbox_inches='tight')
+
+# Save solution graph
+# Note: red nodes are in the set, blue nodes are not
+solution_name = "pipelines_plot_solution.png"
+nx.draw_networkx(k, pos=pos, with_labels=True, node_color='r', font_color='k')
+nx.draw_networkx(othersubgraph, pos=pos, with_labels=True, node_color='b', font_color='w')
+plt.savefig(solution_name, bbox_inches='tight')
+
+print("Your plots are saved to {} and {}".format(original_name, solution_name))
