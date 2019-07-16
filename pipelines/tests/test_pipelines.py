e7fb783b18f297dcaec5e90d664384a0d3c81710
Melody Wong <mwong@dwavesys.com>
2019-07-16 14:19:37 -0700

    Remove _config_found condition
    The master branch should always have access to DWaveSampler, so this _config_found condition is not necessary

--- pipelines/tests/test_pipelines.py
+++ pipelines/tests/test_pipelines.py
@@ -4,18 +4,11 @@ import sys
 
 from dwave.system.samplers import DWaveSampler
 
-try:
-    DWaveSampler()
-    _config_found = True
-except ValueError:
-    _config_found = False
-
 # /tests/integration/test_demo.py
 PY2 = sys.version_info.major == 2
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-@unittest.skipUnless(_config_found, "no configuration found for accessing the D-Wave System")
 class TestDemo(unittest.TestCase):
     def test_smoke(self):
         """run pipelines.py and check that nothing crashes"""
