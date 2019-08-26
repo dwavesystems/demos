d2af72e4eb9251758b93e5bfbf61357002c3ddf5
Melody Wong <mwong@dwavesys.com>
2019-08-26 10:38:50 -0700

    Add 'not' in front of the regex condition

--- maze/demo.py
+++ maze/demo.py
@@ -16,7 +16,7 @@ m = Maze(n_rows, n_cols, start, end, walls)
 bqm = m.get_bqm()
 
 # Submit BQM to a D-Wave sampler
-# Note: when grabbing the solution, we are only grabbing path segments that have
+# Note: when grabbing the path, we are only grabbing path segments that have
 #   been "selected" (i.e. indicated with a 1).
 # Note2: in order to get the BQM at the right energy levels, auxiliary variables
 #   may have been included in the BQM. These auxiliary variables are no longer
@@ -24,10 +24,10 @@ bqm = m.get_bqm()
 #   them out with regex (i.e. re.match(r"^aux(\d+)$", k)])
 sampler = EmbeddingComposite(DWaveSampler())
 result = sampler.sample(bqm, num_reads=1000)
-solution = [k for k, v in result.first.sample.items() if v==1
-            and re.match(r"^aux(\d+)$", k)]
+path = [k for k, v in result.first.sample.items() if v==1
+            and not re.match(r"^aux(\d+)$", k)]
 
 # Visualize maze solution
-m.visualize(solution)
+m.visualize(path)
 print("\n")
 print(result)
