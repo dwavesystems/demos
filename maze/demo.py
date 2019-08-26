48fd4b36ec035edaee81955d2041626f863d925c
Melody Wong <mwong@dwavesys.com>
2019-08-26 10:28:40 -0700

    Filter out auxiliary variables from the sample

--- maze/demo.py
+++ maze/demo.py
@@ -1,6 +1,8 @@
-from maze import get_maze_bqm, Maze
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+import re
+
+from maze import get_maze_bqm, Maze
 
 # Create maze
 n_rows = 2
@@ -14,10 +16,18 @@ m = Maze(n_rows, n_cols, start, end, walls)
 bqm = m.get_bqm()
 
 # Submit BQM to a D-Wave sampler
+# Note: when grabbing the solution, we are only grabbing path segments that have
+#   been "selected" (i.e. indicated with a 1).
+# Note2: in order to get the BQM at the right energy levels, auxiliary variables
+#   may have been included in the BQM. These auxiliary variables are no longer
+#   useful once we have our result. Hence, we can just ignore them by filtering
+#   them out with regex (i.e. re.match(r"^aux(\d+)$", k)])
 sampler = EmbeddingComposite(DWaveSampler())
 result = sampler.sample(bqm, num_reads=1000)
-solution = [k for k, v in result.first.sample.items() if v==1]
+solution = [k for k, v in result.first.sample.items() if v==1
+            and re.match(r"^aux(\d+)$", k)]
 
 # Visualize maze solution
 m.visualize(solution)
+print("\n")
 print(result)
