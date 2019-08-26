9b812d2d2531a934a379aceda60c13c69ecbfeda
Melody Wong <mwong@dwavesys.com>
2019-08-26 10:59:22 -0700

    Make a more interesting maze. Add comments

--- maze/demo.py
+++ maze/demo.py
@@ -5,29 +5,34 @@ import re
 from maze import get_maze_bqm, Maze
 
 # Create maze
-n_rows = 2
-n_cols = 2
+n_rows = 3
+n_cols = 4
 start = '0,0n'
-end = '1,0w'
-walls = ['1,1n']
+end = '2,4w'
+walls = ['1,1n', '2,2w']
 
 # Get BQM
 m = Maze(n_rows, n_cols, start, end, walls)
 bqm = m.get_bqm()
 
 # Submit BQM to a D-Wave sampler
+# Note: the values of num_reads and chain_strength were arbitrarily picked. They
+#   just needed to be slightly higher than the default values in order to get
+#   this demo to produce the correct solution.
+sampler = EmbeddingComposite(DWaveSampler())
+result = sampler.sample(bqm, num_reads=1000, chain_strength=2)
+
+# Interpret result
 # Note: when grabbing the path, we are only grabbing path segments that have
 #   been "selected" (i.e. indicated with a 1).
 # Note2: in order to get the BQM at the right energy levels, auxiliary variables
 #   may have been included in the BQM. These auxiliary variables are no longer
 #   useful once we have our result. Hence, we can just ignore them by filtering
 #   them out with regex (i.e. re.match(r"^aux(\d+)$", k)])
-sampler = EmbeddingComposite(DWaveSampler())
-result = sampler.sample(bqm, num_reads=1000)
 path = [k for k, v in result.first.sample.items() if v==1
             and not re.match(r"^aux(\d+)$", k)]
 
-# Visualize maze solution
+# Visualize maze path
 m.visualize(path)
 print("\n")
-print(result)
+print(result.first.sample)
