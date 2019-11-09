3f082ad6e72ee6cc0a827c3b53bebc9974dc6be9
Melody Wong <mwong@dwavesys.com>
2019-11-08 16:49:45 -0800

    Indent code and add if __name__ line only

--- mutual-information-feature-selection/titanic.py
+++ mutual-information-feature-selection/titanic.py
@@ -67,74 +67,75 @@ def conditional_mutual_information(p, j, *conditional_indices):
             - conditional_shannon_entropy(p, j, *conditional_indices))
 
 
-# Rank the MI between survival and every other variable
-scores = {}
-features = list(set(dataset.columns).difference(('survived',)))
-for feature in features:
-    scores[feature] = mutual_information(prob(dataset[['survived', feature]].values), 0)
+if __name__ == "__main__":
+    # Rank the MI between survival and every other variable
+    scores = {}
+    features = list(set(dataset.columns).difference(('survived',)))
+    for feature in features:
+        scores[feature] = mutual_information(prob(dataset[['survived', feature]].values), 0)
 
-labels, values = zip(*sorted(scores.items(), key=lambda pair: pair[1], reverse=True))
+    labels, values = zip(*sorted(scores.items(), key=lambda pair: pair[1], reverse=True))
 
-# Plot the MI between survival and every other variable
-plt.figure()
-ax1 = plt.subplot(1, 2, 1)
-ax1.set_title("Mutual Information")
-ax1.set_ylabel('MI Between Survival and Feature')
-plt.xticks(np.arange(len(labels)), labels, rotation=90)
-plt.bar(np.arange(len(labels)), values)
+    # Plot the MI between survival and every other variable
+    plt.figure()
+    ax1 = plt.subplot(1, 2, 1)
+    ax1.set_title("Mutual Information")
+    ax1.set_ylabel('MI Between Survival and Feature')
+    plt.xticks(np.arange(len(labels)), labels, rotation=90)
+    plt.bar(np.arange(len(labels)), values)
 
-# The Titanic dataset provides a familiar, intuitive example available in the public
-# domain. In itself, however, it is not a good fit for solving by sampling. Run naively on
-# this dataset, it finds numerous good solutions but is unlikely to find the exact optimal solution.
-# There are many techniques for reformulating problems for the D-Wave system that can
-# improve performance on various metrics, some of which can help narrow down good solutions
-# to closer approach an optimal solution.
-# This demo solves the problem for just the highest-scoring features.
+    # The Titanic dataset provides a familiar, intuitive example available in the public
+    # domain. In itself, however, it is not a good fit for solving by sampling. Run naively on
+    # this dataset, it finds numerous good solutions but is unlikely to find the exact optimal solution.
+    # There are many techniques for reformulating problems for the D-Wave system that can
+    # improve performance on various metrics, some of which can help narrow down good solutions
+    # to closer approach an optimal solution.
+    # This demo solves the problem for just the highest-scoring features.
 
-# Select 8 features with the top MI ranking found above.
-keep = 8
+    # Select 8 features with the top MI ranking found above.
+    keep = 8
 
-sorted_scores = sorted(scores.items(), key=lambda pair: pair[1], reverse=True)
-dataset = dataset[[column[0] for column in sorted_scores[0:keep]] + ["survived"]]
-features = list(set(dataset.columns).difference(('survived',)))
+    sorted_scores = sorted(scores.items(), key=lambda pair: pair[1], reverse=True)
+    dataset = dataset[[column[0] for column in sorted_scores[0:keep]] + ["survived"]]
+    features = list(set(dataset.columns).difference(('survived',)))
 
-# Build a QUBO that maximizes MI between survival and a subset of features
-bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
+    # Build a QUBO that maximizes MI between survival and a subset of features
+    bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
 
-# Add biases as (negative) MI with survival for each feature
-for feature in features:
-    mi = mutual_information(prob(dataset[['survived', feature]].values), 1)
-    bqm.add_variable(feature, -mi)
+    # Add biases as (negative) MI with survival for each feature
+    for feature in features:
+        mi = mutual_information(prob(dataset[['survived', feature]].values), 1)
+        bqm.add_variable(feature, -mi)
 
-# Add interactions as (negative) MI with survival for each set of 2 features
-for f0, f1 in itertools.combinations(features, 2):
-    cmi_01 = conditional_mutual_information(prob(dataset[['survived', f0, f1]].values), 1, 2)
-    cmi_10 = conditional_mutual_information(prob(dataset[['survived', f1, f0]].values), 1, 2)
-    bqm.add_interaction(f0, f1, -cmi_01)
-    bqm.add_interaction(f1, f0, -cmi_10)
+    # Add interactions as (negative) MI with survival for each set of 2 features
+    for f0, f1 in itertools.combinations(features, 2):
+        cmi_01 = conditional_mutual_information(prob(dataset[['survived', f0, f1]].values), 1, 2)
+        cmi_10 = conditional_mutual_information(prob(dataset[['survived', f1, f0]].values), 1, 2)
+        bqm.add_interaction(f0, f1, -cmi_01)
+        bqm.add_interaction(f1, f0, -cmi_10)
 
-bqm.normalize()  # Normalize biases & interactions to the range -1, 1
+    bqm.normalize()  # Normalize biases & interactions to the range -1, 1
 
-# Set up a QPU sampler with a fully-connected graph of all the variables
-qpu_sampler = DWaveSampler(solver={'qpu': True})
+    # Set up a QPU sampler with a fully-connected graph of all the variables
+    qpu_sampler = DWaveSampler(solver={'qpu': True})
 
-embedding = find_clique_embedding(bqm.variables,
-                                  16, 16, 4,  # size of the chimera lattice
-                                  target_edges=qpu_sampler.edgelist)
+    embedding = find_clique_embedding(bqm.variables,
+                                      16, 16, 4,  # size of the chimera lattice
+                                      target_edges=qpu_sampler.edgelist)
 
-sampler = FixedEmbeddingComposite(qpu_sampler, embedding)
+    sampler = FixedEmbeddingComposite(qpu_sampler, embedding)
 
-# For each number of features, k, penalize selection of fewer or more features
-selected_features = np.zeros((len(features), len(features)))
-for k in range(1, len(features) + 1):
-    kbqm = bqm.copy()
-    kbqm.update(dimod.generators.combinations(features, k,
-                                              strength=6))  # Determines the penalty
+    # For each number of features, k, penalize selection of fewer or more features
+    selected_features = np.zeros((len(features), len(features)))
+    for k in range(1, len(features) + 1):
+        kbqm = bqm.copy()
+        kbqm.update(dimod.generators.combinations(features, k,
+                                                  strength=6))  # Determines the penalty
 
-    sample = sampler.sample(kbqm, num_reads=10000).first.sample
+        sample = sampler.sample(kbqm, num_reads=10000).first.sample
 
-    for fi, f in enumerate(features):
-        selected_features[k-1, fi] = sample[f]
+        for fi, f in enumerate(features):
+            selected_features[k-1, fi] = sample[f]
 
 # Plot the best feature set per number of selected features
 ax2 = plt.subplot(1, 2, 2)
